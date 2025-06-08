e89df08e456f985c07bbf1c1712eb1e7a317c326
Sophia Garcia <sophiagarccia2@gmail.com>
2025-06-08 16:56:37 -0300

    adicionando o tolower na questao 4

--- trabalho1/SophiaGuimaraes20242160002.c
+++ trabalho1/SophiaGuimaraes20242160002.c
@@ -315,15 +315,15 @@ int q4(char *strTexto, char *strBusca, int posicoes[30]) {
         int encontrou = 1;
         
         for (j = 0; j < tamBusca; j++) {
-            if (strTexto[i + j] != strBusca[j]) {
-                encontrou = 0;
+ 	if (tolower(strTexto[i + j]) != tolower(strBusca[j])) {       
+		encontrou = 0;
                 break;
             }
         }
 
         if (encontrou) {
-            posicoes[qtdOcorrencias * 2] = i + 1;                 // posição inicial (começa em 1)
-            posicoes[qtdOcorrencias * 2 + 1] = i + tamBusca;      // posição final
+            posicoes[qtdOcorrencias * 2] = i + 1;                
+            posicoes[qtdOcorrencias * 2 + 1] = i + tamBusca;      
             qtdOcorrencias++;
         }
     }
